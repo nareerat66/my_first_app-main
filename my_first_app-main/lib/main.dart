4cbd2316290a3c9444cd41df66f596554be4b611
nareerat66 <nareeratw66@nu.ac.th>
2025-07-29 09:09:22 +0700

    first commit

---
+++ my_first_app-main/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart'; // Import ไลบรารี Material ของ Flutter
+
+void main() {
+  // จุดเริ่มต้นของแอป Flutter
+  runApp(const MyApp()); // บอก Flutter ว่าวิดเจ็ตใดคือรากฐานของแอป
+}
+
+// MyApp โดยทั่วไปคือวิดเจ็ตรากฐานของแอปพลิเคชันของคุณ
+class MyApp extends StatelessWidget {
+  const MyApp({super.key}); // Constructor
+
+  @override
+  Widget build(BuildContext context) {
+    // เมธอด build() ส่งคืนโครงสร้างต้นไม้วิดเจ็ตสำหรับวิดเจ็ตนี้
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold( // โครงสร้างหน้าจอพื้นฐาน
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+          child: Text('Hello, Flutter World!'),
+        ),
+      ),
+    );
+  }
+}
